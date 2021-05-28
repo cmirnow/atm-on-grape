b94511111bf0dfc5b896a8974f731a9320baaddb
rishard <info@masterpro.ws>
2021-05-28 20:49:39 +0300

    code improvement

---
+++ app/services/calculations.rb
@@ -0,0 +1,51 @@
+class Calculations
+  def self.get_banknotes
+    BanknoteData.all.map do |value|
+      [
+        value['banknote1'],
+        value['banknote2'],
+        value['banknote5'],
+        value['banknote10'],
+        value['banknote25'],
+        value['banknote50']
+      ]
+    end
+  end
+
+  def self.subset_sum(*args)
+    args[0].length.downto(1).flat_map do |i|
+      args[0].combination(i).to_a
+    end.select do |a|
+      a.inject(:+) == args[1]
+    end
+  end
+
+  def self.counting(*args)
+    tmp = []
+    [1, 2, 5, 10, 25, 50].map.with_index { |d, index| args[0][index].times { tmp << d } }
+    tmp
+  end
+
+  def self.get_money(*args)
+    answer = subset_sum(counting(get_banknotes.flatten), args[0].to_i).first
+    begin
+      given_away = { 1 => 0, 2 => 0, 5 => 0, 10 => 0, 25 => 0, 50 => 0 }.merge(Hash[
+answer.group_by { |i| i }.map do |k, v|
+  [k, v.count]
+end
+  ]).values
+      # reduce the number of banknotes:
+      BanknoteData.update({
+                            banknote1: BanknoteData.all[0][:banknote1] - given_away[0],
+                            banknote2: BanknoteData.all[0][:banknote2] - given_away[1],
+                            banknote5: BanknoteData.all[0][:banknote5] - given_away[2],
+                            banknote10: BanknoteData.all[0][:banknote10] - given_away[3],
+                            banknote25: BanknoteData.all[0][:banknote25] - given_away[4],
+                            banknote50: BanknoteData.all[0][:banknote50] - given_away[5]
+                          })
+      answer
+    rescue StandardError
+      'transaction not allowed'
+    end
+  end
+end
