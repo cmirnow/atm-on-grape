8e3b8d506d6e79cca0a4d5291347e5e884531b22
rishard <info@masterpro.ws>
2021-06-10 04:47:33 +0300

    adding tests

--- test/all_tests.rb
+++ test/all_tests.rb
@@ -10,7 +10,17 @@ class Banknote::AssetTest < ActiveSupport::TestCase
 
   test 'GET /api/v1/banknote_asset' do
     get('/api/v1/banknote_asset')
-
     assert last_response.ok?
+    assert_equal 200, last_response.status
+  end
+
+  test 'Successful cash withdrawal' do
+    patch('/api/v1/banknote_asset?sum=93')
+    assert_equal '[1, 2, 5, 10, 25, 50]', last_response.body
+  end
+
+  test 'Unable to issue required amount' do
+    patch('/api/v1/banknote_asset?sum=94')
+    assert_equal 'transaction not allowed', last_response.body
   end
 end
